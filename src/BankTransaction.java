d2ded9c6f49c78d4b7759a6ae73454caeba2915e
varsha2031997 <kv2031997@gmail.com>
2025-08-09 10:20:53 +0530

    Update BankTransaction.java

--- src/BankTransaction.java
+++ src/BankTransaction.java
@@ -1,67 +1,51 @@
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 
-//Sort Bank Transactions by Amount and Date
-//
-//Class: Transaction (txnId, amount, txnDate)
-//
-//Sort: higher amount first; if equal, most recent txn first.
-public class BankTransaction {
+public class Example {
     String txnId;
     double amount;
     LocalDate date;
 
+    Example(String txnId, double amount, LocalDate date) {
+        this.txnId = txnId;
+        this.amount = amount;
+        this.date = date;
+    }
+
     @Override
     public String toString() {
-        return "BankTransaction{" +
+        return "Example{" +
                 "txnId='" + txnId + '\'' +
                 ", amount=" + amount +
                 ", date=" + date +
                 '}';
     }
 
-    BankTransaction(String txnId,double amount,LocalDate date)
-    {
-        this.txnId=txnId;
-        this.amount=amount;
-        this.date=date;
-    }
-
-
-}
-
-class Transactions implements Comparator<BankTransaction>
-{
-
-    @Override
-    public int compare(BankTransaction o1, BankTransaction o2) {
-        int amountCompare = Double.compare(o1.amount, o2.amount);
-        if(amountCompare ==0)
-        {
-            return o1.date.compareTo(o2.date);
-        }
-        return amountCompare;
-    }
-}
-class BankingTransaction
-{
     public static void main(String[] args) {
-        List<BankTransaction> bankdetails = new ArrayList<>();
-        bankdetails.add(new BankTransaction("txnId123",100000, LocalDate.of(25,07,10)));
-        bankdetails.add(new BankTransaction("txnId456",200000, LocalDate.of(25,07,11)));
-        bankdetails.add(new BankTransaction("txnId890",500000, LocalDate.of(25,07,13)));
-        bankdetails.add(new BankTransaction("txnId100",500000, LocalDate.of(25,07,12)));
 
-        Collections.sort(bankdetails,new Transactions());
+        ArrayList<Example> bank = new ArrayList<>();
+        bank.add(new Example("txnId123", 10000, LocalDate.of(2025, 7, 23)));
+        bank.add(new Example("txnId124", 20000, LocalDate.of(2025, 7, 22)));
+        bank.add(new Example("txnId126", 50000, LocalDate.of(2025, 7, 21)));
+        bank.add(new Example("txnId127", 1000, LocalDate.of(2025, 7, 20)));
+        bank.add(new Example("txnId129", 1000, LocalDate.of(2025, 7, 31)));
 
-        for(BankTransaction trans : bankdetails)
-        {
-            System.out.println(trans);
+        // Modern Java 8+ comparator: higher amount first, then most recent date first
+        Comparator<Example> amtDateCompare =
+                Comparator.comparingDouble((Example e) -> e.amount).reversed()
+                          .thenComparing(Example::getDate, Comparator.reverseOrder());
+
+        Collections.sort(bank, amtDateCompare);
+
+        for (Example banks : bank) {
+            System.out.println(banks);
         }
+    }
 
+    // Getter for date (needed for method reference)
+    public LocalDate getDate() {
+        return date;
     }
 }
