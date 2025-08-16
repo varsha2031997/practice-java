3b70521562c18f7298fda3e31e9664e53f706a4a
varshini <varshini.k@myn.team>
2025-08-16 16:05:12 +0530

    Pali

--- src/StringVowels.java
+++ src/StringVowels.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+
 public class StringVowels {
     public static void main(String[] args) {
-        String str = "abcdefgh";
-        for(int i=0;i<=str.length()-1;i++)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String");
+        String str = sc.nextLine();
+        int vowelsCount=0;int consonantCount = 0;
+
+        for(int i=0;i<str.length();i++)
         {
+            char ch = str.charAt(i);
+            if(Character.isLetter(ch))
+            {
+                if("aeiouAEIOU".contains(String.valueOf(ch)))
+                {
+                    vowelsCount++;
+                }else {
+                    consonantCount++;
+                }
+
+            }
+
 
         }
+
+        System.out.println("Vowels count :"+vowelsCount+"Consonant count : "+consonantCount);
+
     }
 }
